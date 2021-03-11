9744ed41bee37bf21a0e6827d427e1f3bd5119d0
Hunt J. Sparra <>
2021-03-11 17:31:17 -0500

    Added card counter psuedo-code (commented out because it has not been tested)

---
+++ TeamiumPremium/CardCounter.java
@@ -0,0 +1,21 @@
+// package TeamiumPremium;
+
+// import ProjectTwoEngine.Monster;
+
+// public class CardCounter {
+//     public int[] numCards;
+
+//     public CardCounter() {
+//         // Needs to be updated if Prof. Lepinski changes
+//         // DeckFactory.createDeck()
+//         numCards = new int[] { 2, 4, 5, 5, 4, 2 };
+//     }
+
+//     public cardDrawn(Monster drawnMonster) {
+//         numCards[drawnMonster] -= 1;
+//     }
+
+//     public getRemainingMonsterOfType(Monster monsterType) {
+//         return numCards[monsterType];
+//     }
+// }
